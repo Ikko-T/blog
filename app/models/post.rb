663e9b7d020fa34ee287e35a33463e21ceaa671d
Ikko-T <skyblueboy32@gmail.com>
2022-01-17 22:13:57 +0900

    Create a new posting functionality.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  # validates :title, presence: true, length: { maximum: 50, too_long: "最大%{count}文字まで使えます"}
+  # validates :content, presence: true, length: { maximum: 500, too_long: "最大%{count}文字まで使えます" }
 end
